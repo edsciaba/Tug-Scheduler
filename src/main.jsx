53c17913301944cfd0bdd06f74c2054e51448e32
edsciaba <ed.sciaba@gmail.com>
2025-05-07 16:55:07 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TugboatScheduler from './TugboatScheduler.jsx';
+import TugboatScheduler from "./TugboatScheduler.jsx";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
