4eaaebed153d15afec1d07d0e71ee0663df17ceb
edsciaba <ed.sciaba@gmail.com>
2025-05-07 17:05:57 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TugboatScheduler from "./TugboatScheduler.jsx";
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TugboatScheduler from "./TugboatScheduler.jsx"; 
+import "./index.css"; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TugboatScheduler />
   </React.StrictMode>
